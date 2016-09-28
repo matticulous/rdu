a53fa9204154c29f4a6a00f07eb5f93909d5814d
Matt Clark <matt@matticulous.com>
2016-09-27 23:14:34 -0400

    Added additional file testing and exception handling.

--- rdu.rb
+++ rdu.rb
@@ -5,32 +5,40 @@ require 'find'
 require 'json'
 
 options = {}
-#default directory location of home dir?
+# Parse program flags and switches
 op = OptionParser.new do |opts|
+  # Directory to scan
   opts.on("-d DIR", "Directory containing files to list") do |dir|
     options[:dir] = dir
   end
+  # Print output as a condensed JSON string, useable by consuming programs
   opts.on("-u", "--ugly", "Print output as JSON string") do
     options[:ugly] = true
   end
 end
 op.parse!
 
-if options.empty?
+# A directory to scan must be provided
+if !options[:dir]
   STDERR.puts "Error: you must supply a directory"
   puts op.help
   exit(1)
 end
 
 file_hash = { :files => [] }
-Find.find(options[:dir]) do |path|
-  if !FileTest.directory?(path)
-    file_hash[:files] << { path => FileTest.size(path)}
+begin
+  # Build a hash of files keyed by path with a value of size in bytes
+  Find.find(options[:dir]) do |path|
+    if !FileTest.directory?(path) && FileTest.exist?(path)
+      file_hash[:files] << { path => FileTest.size(path)}
+    end
   end
-end
-
-if options[:ugly]
-  puts JSON.generate(file_hash)
-else
-  puts JSON.pretty_generate(file_hash)
+  # Print the files hash to JSON
+  if options[:ugly]
+    puts JSON.generate(file_hash)
+  else
+    puts JSON.pretty_generate(file_hash)
+  end
+rescue Exception => e
+  puts e.message
 end
